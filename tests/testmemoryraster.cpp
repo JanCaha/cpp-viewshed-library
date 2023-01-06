05921a0ffff5d3c09b7b79a6b444101327cc911c
Jan Caha <jan.caha@outlook.com>
2023-01-06 10:54:17 +0100

    update tests

--- tests/testmemoryraster.cpp
+++ tests/testmemoryraster.cpp
@@ -22,30 +22,38 @@ class TestMemoryRaster : public QObject
     {
         MemoryRaster mr( dem );
         QVERIFY( mr.isValid() );
+        QVERIFY( mr.error().isEmpty() );
         QVERIFY( mr.crs() == dem->crs() );
         QVERIFY( mr.extent() == dem->extent() );
         QVERIFY( mr.height() == dem->height() );
         QVERIFY( mr.width() == dem->width() );
         QVERIFY( mr.dataType() == Qgis::DataType::Float32 );
         QVERIFY( mr.defaultBand() == 1 );
-        QVERIFY( mr.dataProvider()->isValid() );
-        QVERIFY( mr.dataProvider()->dataSourceUri().contains( QString( "/vsimem/" ) ) );
-        QVERIFY( mr.dataProvider()->block( 1, mr.extent(), mr.width(), mr.height() )->isValid() );
-        QVERIFY( mr.dataProvider()
-                     ->block( 1, mr.extent(), mr.width(), mr.height() )
-                     ->value( (int)mr.height() / 2, (int)mr.width() / 2 ) == 0.0 );
+
+        std::unique_ptr<QgsRasterDataProvider> dp = mr.dataProvider();
+        QVERIFY( dp->isValid() );
+        QVERIFY( dp->dataSourceUri().contains( QString( "/vsimem/" ) ) );
+        QVERIFY( dp->block( 1, mr.extent(), mr.width(), mr.height() )->isValid() );
+
+        std::shared_ptr<QgsRasterBlock> rb( dp->block( 1, mr.extent(), mr.width(), mr.height() ) );
+        QVERIFY( rb->value( (int)mr.height() / 2, (int)mr.width() / 2 ) ==
+                 dem->dataProvider()->sourceNoDataValue( 1 ) );
     }
 
     void constructWithSettings()
     {
-        MemoryRaster mr( dem, Qgis::DataType::Int32, -1 );
+        double defaultValue = -1;
+        MemoryRaster mr( dem, Qgis::DataType::Int32, defaultValue );
         QVERIFY( mr.isValid() );
         QVERIFY( mr.dataType() == Qgis::DataType::Int32 );
-        QVERIFY( mr.dataProvider()->isValid() );
-        QVERIFY( mr.dataProvider()->dataSourceUri().contains( QString( "/vsimem/" ) ) );
-        QVERIFY( mr.dataProvider()
-                     ->block( 1, mr.extent(), mr.width(), mr.height() )
-                     ->value( (int)mr.height() / 2, (int)mr.width() / 2 ) == -1 );
+
+        std::unique_ptr<QgsRasterDataProvider> dp = mr.dataProvider();
+        QVERIFY( dp->isValid() );
+        QVERIFY( dp->dataSourceUri().contains( QString( "/vsimem/" ) ) );
+
+        std::shared_ptr<QgsRasterBlock> rb( dp->block( 1, mr.extent(), mr.width(), mr.height() ) );
+        double value = rb->value( (int)mr.height() / 2, (int)mr.width() / 2 );
+        QVERIFY( rb->value( (int)mr.height() / 2, (int)mr.width() / 2 ) == defaultValue );
     }
 
     void setValue()
@@ -55,38 +63,6 @@ class TestMemoryRaster : public QObject
         QVERIFY( mr.dataProvider()
                      ->block( 1, mr.extent(), mr.width(), mr.height() )
                      ->value( (int)mr.height() / 2, (int)mr.width() / 2 ) == -100 );
-    }
-
-    void setValues()
-    {
-        MemoryRaster mr( dem );
-
-        std::shared_ptr<QgsRasterBlock> values =
-            std::make_shared<QgsRasterBlock>( mr.dataType(), mr.width(), mr.height() );
-
-        for ( int i = 0; i < values->height(); i++ )
-        {
-            for ( int j = 0; j < values->width(); j++ )
-            {
-                values->setValue( i, j, -1000 );
-            }
-        }
-
-        bool result = mr.setValues( values );
-
-        QVERIFY( result );
-
-        QVERIFY( mr.dataProvider()
-                     ->block( 1, mr.extent(), mr.width(), mr.height() )
-                     ->value( (int)mr.height() / 2, (int)mr.width() / 2 ) == -1000 );
-
-        QVERIFY( mr.dataProvider()
-                     ->block( 1, mr.extent(), mr.width(), mr.height() )
-                     ->value( (int)mr.height() / 4, (int)mr.width() / 4 ) == -1000 );
-
-        QVERIFY( mr.dataProvider()
-                     ->block( 1, mr.extent(), mr.width(), mr.height() )
-                     ->value( (int)mr.height() / 10, (int)mr.width() / 10 ) == -1000 );
     }
 };
 
