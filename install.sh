6d467569b2a2d85398ececfd8d40cb7aae2cc733
Jan Caha <jan.caha@outlook.com>
2023-01-09 21:50:51 +0100

    build also DEB file

--- install.sh
+++ install.sh
@@ -1,4 +1,9 @@
-sudo /usr/bin/cmake --build /home/cahik/Codes/C++/ViewshedLibrary/build --config Release --target install --
+sudo /usr/bin/cmake --build /home/cahik/Codes/C++/ViewshedLibrary/build --config Release # --target install --
+cd build
+cpack -G DEB
+cd ../_packages
+sudo dpkg -i viewshed_*_amd64.deb
+
 # cd build
 # cmake .. 
 # make 
