1b78356f026b57a739a263bb9dd3e0bb105e4b18
Jan Caha <jan.caha@outlook.com>
2023-01-13 13:06:13 +0100

    add results

--- tests/testsettings.in.h
+++ tests/testsettings.in.h
@@ -2,6 +2,7 @@
 #define TEST_SETTINGS_H_IN
 
 #define TEST_DATA_DIR "@TEST_DATA_DIR@"
+#define TEST_DATA_RESULTS_DIR "@TEST_DATA_DIR@/results"
 #define TEST_DATA_DSM "@TEST_DATA_DIR@/dsm.tif"
 
 #endif // TEST_SETTINGS_H_IN
