4ff8bcec2b7f039b5643880b5e13818d58edac25
Jan Caha <jan.caha@outlook.com>
2023-01-14 21:01:40 +0100

    csv

--- tests/testsettings.in.h
+++ tests/testsettings.in.h
@@ -4,5 +4,6 @@
 #define TEST_DATA_DIR "@TEST_DATA_DIR@"
 #define TEST_DATA_RESULTS_DIR "@TEST_DATA_DIR@/results"
 #define TEST_DATA_DSM "@TEST_DATA_DIR@/dsm.tif"
+#define TEST_DATA_LOS "@TEST_DATA_DIR@/los.csv"
 
 #endif // TEST_SETTINGS_H_IN
