089a3099db195c51dcbaeb888be0ba0450ea8fa1
JanCaha <jan.caha@outlook.com>
2023-01-23 14:20:50 +0100

    fixes

--- src/utils/utils.cpp
+++ src/utils/utils.cpp
@@ -35,15 +35,14 @@ void Utils::saveToCsv( std::vector<std::pair<double, double>> data, std::string 
     resultCsvFile.close();
 }
 
-std::vector<std::pair<double, double>> Utils::distanceElevation( std::shared_ptr<LoS> los )
+std::vector<std::pair<double, double>> Utils::distanceElevation( std::shared_ptr<ILoS> los )
 {
     std::vector<std::pair<double, double>> data;
 
     data.push_back( std::make_pair<double, double>( 0, los->vp()->totalElevation() ) );
 
-    for ( int i = 0; i < los->size(); i++ )
+    for ( int i = 0; i < los->numberOfNodes(); i++ )
     {
-        LoSNode ln = los->at( i );
         data.push_back( std::make_pair<double, double>( los->distance( i ), los->elevation( i ) ) );
     }
 
