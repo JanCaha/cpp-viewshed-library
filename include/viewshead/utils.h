440e76fd4837e0f6977fd36550836ff24902260d
JanCaha <jan.caha@outlook.com>
2023-01-23 14:19:47 +0100

    readability

--- include/viewshead/utils.h
+++ include/viewshead/utils.h
@@ -11,7 +11,9 @@ namespace viewshed
     {
       public:
         static void saveToCsv( std::vector<std::pair<double, double>> data, std::string header, std::string fileName );
-        static std::vector<std::pair<double, double>> distanceElevation( std::shared_ptr<LoS> los );
+
+        static std::vector<std::pair<double, double>> distanceElevation( std::shared_ptr<ILoS> los );
+
         static std::vector<std::pair<double, double>> rasterCoordinates( std::shared_ptr<std::vector<LoSNode>> los,
                                                                          LoSNode poi );
     };
