039154202a06ed2ba3f78ec0d0c5b37d7e99dfd9
Jan Caha <jan.caha@outlook.com>
2023-01-25 23:08:06 +0100

    negative indexes and functions to check horizons

--- src/structures/losimportantvalues.cpp
+++ src/structures/losimportantvalues.cpp
@@ -8,11 +8,15 @@ void LoSImportantValues::reset()
 {
     mMaxGradientBefore = -180;
     mMaxGradient = -180;
-    mIndexPoi = 0;
-    mIndexMaxGradientBefore = 0;
-    mIndexMaxGradient = 0;
-    mIndexHorizonBefore = 0;
-    mIndexHorizon = 0;
+    mIndexPoi = -1;
+    mIndexMaxGradientBefore = -1;
+    mIndexMaxGradient = -1;
+    mIndexHorizonBefore = -1;
+    mIndexHorizon = -1;
     mCountHorizonBefore = 0;
     mCountHorizon = 0;
 }
+
+bool LoSImportantValues::horizonBeforeExist() { return mIndexHorizonBefore != -1; }
+
+bool LoSImportantValues::horizonExist() { return mIndexHorizon != -1; }
