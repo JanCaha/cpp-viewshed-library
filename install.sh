804fcf8206b0110caa6493b6df34d72ec0782368
Jan Caha <jan.caha@outlook.com>
2023-01-27 12:59:04 +0100

    update

--- install.sh
+++ install.sh
@@ -1,12 +1,19 @@
 sudo apt purge -y viewshed
+sudo rm /usr/local/lib/libviewshed*
+sudo rm -rf /usr/local/include/viewshed
+sudo rm -rf /usr/local/lib/cmake/viewshed
 
-sudo /usr/bin/cmake --build /home/cahik/Codes/C++/ViewshedLibrary/build --config Release # --target install --
+cmake -S. -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release
+cmake --build build --config Release --target all
+
+cd build/tests 
+ctest -VV
+cd ../..
+
 cd build
-cpack -G DEB
-cd ../_packages
-sudo dpkg -i viewshed_*_amd64.deb
+cpack -G DEB -C Release
+# cd ../_packages
+# sudo dpkg -i viewshed_*_amd64.deb
+cd ..
 
-# cd build
-# cmake .. 
-# make 
-# sudo make install
+sudo cmake --install build --config Release
