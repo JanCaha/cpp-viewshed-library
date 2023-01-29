6f4b7942ad37749dcd710e1c3db8b7623821eeb4
Jan Caha <jan.caha@outlook.com>
2023-01-29 22:26:42 +0100

    document class

--- include/viewshead/abstractlos.h
+++ include/viewshead/abstractlos.h
@@ -17,8 +17,8 @@ using viewshed::ValueType;
 namespace viewshed
 {
     /**
-     * @brief Class that represent line-of-sight. Consists of LoSNodes, view point and target point (both with potential
-     * offset from the surface).
+     * @brief Abstract class that represent line-of-sight (LoS). Consists of LoSNodes, view point and target point (both
+     * with potential offset from the surface).
      *
      */
     class AbstractLoS
@@ -50,42 +50,168 @@ namespace viewshed
          * @param observerOffset
          */
         void setTargetPoint( std::shared_ptr<LoSNode> poi, double targetOffset = 0 );
+        /**
+         * @brief Set the horizontal angle for extracting elevations, distances and gradients from line-of-sight.
+         *
+         * @param angle
+         */
         void setAngle( double angle );
 
+        /**
+         * @brief Prepare line-of-sight for calculation. For example inverse LoS needs some preprocessing before being
+         * able to be analysed.
+         *
+         */
         virtual void prepareForCalculation() = 0;
 
+        /**
+         * @brief Extract gradient value for line-of-sight LoSNode at position `i`.
+         *
+         * @param i
+         * @return double
+         */
         virtual double gradient( int i ) = 0;
+
+        /**
+         * @brief Extract distance for line-of-sight LoSNode at position `i`.
+         *
+         * @param i
+         * @return double
+         */
         virtual double distance( int i ) = 0;
+
+        /**
+         * @brief Extract elevation for line-of-sight LoSNode at position `i`.
+         *
+         * @param i
+         * @return double
+         */
         virtual double elevation( int i ) = 0;
+
         virtual bool isValid() = 0;
 
+        /**
+         * @brief Number of LoSNodes in this line-of-sight.
+         *
+         * @return int
+         */
         virtual int numberOfNodes() = 0;
+
+        /**
+         * @brief Index of target point in the line-of-sight.
+         *
+         * @return int
+         */
         virtual int targetPointIndex() = 0;
+
+        /**
+         * @brief Row number where the outcome of LoS analysis should be stored.
+         *
+         * @return int
+         */
         virtual int resultRow() = 0;
+
+        /**
+         * @brief Column number where the outcome of LoS analysis should be stored.
+         *
+         * @return int
+         */
         virtual int resultCol() = 0;
 
+        /**
+         * @brief LoSNode at specific index.
+         *
+         * @param i
+         * @return LoSNode
+         */
         virtual LoSNode nodeAt( int i ) = 0;
 
+        /**
+         * @brief Horizontal angle for this line-of-sight.
+         *
+         * @return double
+         */
         double horizontalAngle();
 
+        /**
+         * @brief Row number for the target of LoS.
+         *
+         * @return int
+         */
         int targetRow();
+
+        /**
+         * @brief Column number for the target of LoS.
+         *
+         * @return int
+         */
         int targetCol();
 
+        /**
+         * @brief Distance of target.
+         *
+         * @return double
+         */
         double targetDistance();
+
+        /**
+         * @brief Gradient of target.
+         *
+         * @return double
+         */
         double targetGradient();
+
+        /**
+         * @brief Elevation of target.
+         *
+         * @return double
+         */
         double targetElevation();
 
+        /**
+         * @brief View point for this line-of-sight.
+         *
+         * @return std::shared_ptr<Point>
+         */
         std::shared_ptr<Point> vp();
 
       protected:
         AbstractLoS();
 
+        /**
+         * @brief Horizontal angle between view point and target point.
+         *
+         */
         double mAngleHorizontal;
+
+        /**
+         * @brief Distance between observer and target point.
+         *
+         */
         double mPointDistance;
+
+        /**
+         * @brief View point.
+         *
+         */
         std::shared_ptr<Point> mVp = std::make_shared<Point>();
+
+        /**
+         * @brief Target point.
+         *
+         */
         std::shared_ptr<Point> mTp = std::make_shared<Point>();
+
+        /**
+         * @brief Index of target point.
+         *
+         */
         int mTargetIndex;
 
+        /**
+         * @brief Sort LoSNodes by distance from View point.
+         *
+         */
         virtual void sort() = 0;
     };
 
