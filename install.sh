880f356b9910e56427a141470535125823b578fb
Jan Caha <jan.caha@outlook.com>
2023-02-01 19:08:40 +0100

    install commands

--- install.sh
+++ install.sh
@@ -1,13 +1,17 @@
 # sudo apt purge -y viewshed
+# sudo cmake --build build --target uninstall
 
-sudo cmake --build build --target uninstall
+cmake -S. -Bbuild -G Ninja -DCMAKE_BUILD_TYPE=Release -DPACK_DEB:bool=off -DBUILD_DOCUMENTATION:bool=on -DBUILD_TESTS:bool=on
 
-cmake -S. -Bbuild -G Ninja -DCMAKE_BUILD_TYPE=Release -DPACK_DEB=off
 cmake --build build --config Release --target viewshed
 
 # testing
-cmake --build build --config Release --target build_tests
 cmake --build build --config Release --target run_tests
 
 # install
-cmake --build build --config Release --target install
+sudo cmake --build build --config Release --target install
+
+# pack DEB
+# cmake -S. -Bbuild -G Ninja -DCMAKE_BUILD_TYPE=Release -DPACK_DEB:bool=on -DBUILD_DOCUMENTATION:bool=on -DBUILD_TESTS:bool=on
+# cmake --build build --config Release --target pack_deb
+
