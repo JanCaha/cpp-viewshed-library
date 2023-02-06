01d707b52315a916daca17a17febc2ae3a88d4c0
JanCaha <jan.caha@outlook.com>
2023-02-06 09:56:30 +0100

    new widget to simplify

---
+++ src/bin/pointwidget.cpp
@@ -0,0 +1,66 @@
+#include "QHBoxLayout"
+
+#include "pointwidget.h"
+
+PointWidget::PointWidget( QWidget *parent ) : QWidget( parent )
+{
+
+    QHBoxLayout *layout = new QHBoxLayout( this );
+    layout->setContentsMargins( 0, 0, 0, 0 );
+    setLayout( layout );
+
+    mDoubleValidator = new QgsDoubleValidator( this );
+
+    mPointX = new QLineEdit( this );
+    mPointX->setValidator( mDoubleValidator );
+    mPointY = new QLineEdit( this );
+    mPointY->setValidator( mDoubleValidator );
+
+    connect( mPointY, &QLineEdit::textChanged, this, &PointWidget::updatePoint );
+    connect( mPointY, &QLineEdit::textChanged, this, &PointWidget::updatePoint );
+
+    layout->addWidget( mPointX );
+    layout->addWidget( mPointY );
+}
+
+void PointWidget::updatePoint()
+{
+    mPointValid = false;
+
+    QString text = mPointX->text();
+
+    if ( mDoubleValidator->validate( text ) == QgsDoubleValidator::Acceptable )
+    {
+        mPoint.setX( QgsDoubleValidator::toDouble( text ) );
+    }
+    else
+        return;
+
+    text = mPointY->text();
+
+    if ( mDoubleValidator->validate( text ) == QgsDoubleValidator::Acceptable )
+    {
+        mPoint.setY( QgsDoubleValidator::toDouble( text ) );
+    }
+    else
+        return;
+
+    mPointValid = true;
+
+    emit pointChanged( point() );
+    emit pointXYChanged( pointXY() );
+}
+
+bool PointWidget::isPointValid() { return mPointValid; }
+
+QgsPoint PointWidget::point() { return mPoint; };
+
+QgsPointXY PointWidget::pointXY() { return QgsPointXY( mPoint.x(), mPoint.y() ); };
+
+void PointWidget::setXY( double x, double y )
+{
+    mPointX->setText( QVariant( x ).toString() );
+    mPointY->setText( QVariant( y ).toString() );
+}
+
+void PointWidget::setPoint( QgsPoint p ) { setXY( p.x(), p.y() ); }
---
+++ src/bin/pointwidget.h
@@ -0,0 +1,38 @@
+#ifndef POINTWIDGET_H
+#define POINTWIDGET_H
+
+#include <QLineEdit>
+#include <QWidget>
+
+#include "qgsdoublevalidator.h"
+#include "qgspoint.h"
+#include "qgspointxy.h"
+
+class PointWidget : public QWidget
+{
+    Q_OBJECT
+
+  public:
+    PointWidget( QWidget *parent = nullptr );
+
+    bool isPointValid();
+    QgsPoint point();
+    QgsPointXY pointXY();
+    void setXY( double x, double y );
+    void setPoint( QgsPoint p );
+
+  signals:
+    void pointChanged( QgsPoint point );
+    void pointXYChanged( QgsPointXY point );
+
+  private:
+    void updatePoint();
+
+    QLineEdit *mPointX = nullptr;
+    QLineEdit *mPointY = nullptr;
+    QgsDoubleValidator *mDoubleValidator = nullptr;
+    QgsPoint mPoint;
+    bool mPointValid;
+};
+
+#endif
