15aa029c389f4037144f7b7f38d3e159ed59500b
Jan Caha <jan.caha@outlook.com>
2023-02-23 22:39:17 +0100

    add functions

--- src/bin/commandlinehelper.h
+++ src/bin/commandlinehelper.h
@@ -4,6 +4,7 @@
 
 #include <QCommandLineParser>
 #include <QDebug>
+#include <QString>
 
 int exitWithError( const char *error, QCommandLineParser &parser )
 {
@@ -23,4 +24,105 @@ void printProgressInfo( int size, int i )
                   << "     "
                   << "\r" << std::flush;
     }
+}
+
+void addEarthDiameter( QCommandLineParser &parser )
+{
+    QCommandLineOption earthDiameter( QStringLiteral( "earthDiameter" ), "Earth diameter.", "12740000" );
+    parser.addOption( earthDiameter );
+}
+
+double getEarthDiameter( QCommandLineParser &parser )
+{
+    return QVariant( parser.value( QStringLiteral( "earthDiameter" ) ) ).toDouble();
+}
+
+void addCurvatureCorrections( QCommandLineParser &parser )
+{
+    QCommandLineOption useCurvatureCorrections( QStringLiteral( "useCurvatureCorrections" ),
+                                                "Use curvature corrections?", "true" );
+    parser.addOption( useCurvatureCorrections );
+}
+
+bool getCurvatureCorrections( QCommandLineParser &parser )
+{
+    return QVariant( parser.value( QStringLiteral( "useCurvatureCorrections" ) ) ).toBool();
+}
+
+void addRefractionCoefficient( QCommandLineParser &parser )
+{
+    QCommandLineOption refractionCoefficient( QStringLiteral( "refractionCoefficient" ), "Refraction coefficient.",
+                                              "0.142860" );
+    parser.addOption( refractionCoefficient );
+}
+
+double getRefractionCoefficient( QCommandLineParser &parser )
+{
+    return QVariant( parser.value( QStringLiteral( "refractionCoefficient" ) ) ).toDouble();
+}
+
+void addDem( QCommandLineParser &parser )
+{
+    QCommandLineOption dem( QStringLiteral( "dem" ), "Raster file representing DEM for viewshed calculation.", " " );
+    parser.addOption( dem );
+}
+
+void addResultFolder( QCommandLineParser &parser )
+{
+    QCommandLineOption resultsFolder( QStringLiteral( "resultsFolder" ), "Output folder to store the results in.",
+                                      " " );
+    parser.addOption( resultsFolder );
+}
+
+void addHeightObserver( QCommandLineParser &parser )
+{
+    QCommandLineOption heightObserver( QStringLiteral( "heightObserver" ), "Height of the observer.", " " );
+    parser.addOption( heightObserver );
+}
+
+double getHeightObserver( QCommandLineParser &parser )
+{
+    return QVariant( parser.value( QStringLiteral( "heightObserver" ) ) ).toDouble();
+}
+
+void addHeightTarget( QCommandLineParser &parser )
+{
+    QCommandLineOption heightTarget( QStringLiteral( "heightTarget" ), "Height of the target.", "0" );
+    parser.addOption( heightTarget );
+}
+
+double getHeightTarget( QCommandLineParser &parser )
+{
+    return QVariant( parser.value( QStringLiteral( "heightTarget" ) ) ).toDouble();
+}
+
+struct Coord
+{
+    double x, y;
+};
+
+Coord getCoords( QCommandLineParser &parser, QString paramName )
+{
+    QString coords( parser.value( paramName ) );
+
+    Coord coord;
+    if ( coords.contains( ";" ) )
+    {
+        coord.x = coords.split( ";" )[0].toDouble();
+        coord.y = coords.split( ";" )[1].toDouble();
+    }
+
+    return coord;
+}
+
+QString resultFolderAbsolute( QCommandLineParser &parser )
+{
+    QDir output( parser.value( QStringLiteral( "resultsFolder" ) ) );
+
+    if ( !output.exists() )
+    {
+        exitWithError( "Error: Output folder does not exist.", parser );
+    }
+
+    return output.absolutePath();
 }
