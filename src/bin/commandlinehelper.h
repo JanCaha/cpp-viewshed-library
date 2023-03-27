cf7bfde10a54db781a79b0ced927a956e6a46d3d
JanCaha <jan.caha@outlook.com>
2023-03-27 12:47:22 +0200

    mask visibility

--- src/bin/commandlinehelper.h
+++ src/bin/commandlinehelper.h
@@ -139,3 +139,31 @@ QString getDemFilePath( QCommandLineParser &parser )
 
     return demFile.fileName();
 }
+
+void addVisibilityMask( QCommandLineParser &parser )
+{
+    QCommandLineOption visibilityMask( QStringLiteral( "visibilityMask" ),
+                                       "Raster file representing visibility mask, specifing areas to calculate "
+                                       "visibility in, for viewshed calculation.",
+                                       " " );
+    parser.addOption( visibilityMask );
+}
+
+QString getVisibilityMask( QCommandLineParser &parser )
+{
+    QString path = parser.value( QStringLiteral( "visibilityMask" ) );
+
+    if ( path.trimmed().isEmpty() )
+    {
+        return QString();
+    }
+
+    QFile file( path );
+
+    if ( !file.exists() )
+    {
+        exitWithError( "Error: Dem file does not exist.", parser );
+    }
+
+    return file.fileName();
+}
