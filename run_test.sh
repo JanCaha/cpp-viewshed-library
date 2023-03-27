6a2feb9ae0a26781768f34e0517d020a023aa77b
JanCaha <jan.caha@outlook.com>
2023-03-27 14:03:50 +0200

    run test from command line

---
+++ run_test.sh
@@ -0,0 +1,8 @@
+build/src/bin/viewshed \
+    --dem /home/cahik/Codes/C++/cpp-viewshed-library/tests/data/dsm.tif \
+    --resultsFolder "/tmp/viewshed" --observerPosition "-336428.767;-1189102.785" \
+    --heightObserver "1.6" \
+    --refractionCoefficient "0.142860" \
+    --useCurvatureCorrections "true" \
+    --earthDiameter "12754794.310000" \
+    --visibilityMask /home/cahik/Codes/C++/cpp-viewshed-library/tests/data/visiblity_mask.tif
