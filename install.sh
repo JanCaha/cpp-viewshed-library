58c25f0c2e638bc4eeb8f5dbef0061a46608ed07
Jan Caha <jan.caha@outlook.com>
2023-04-01 09:10:09 +0200

    turn off

--- install.sh
+++ install.sh
@@ -1,13 +1,13 @@
 # sudo apt purge -y viewshed
 # sudo cmake --build build --target uninstall
 
-cmake -S. -Bbuild -G Ninja -DCMAKE_BUILD_TYPE=Release -DPACK_DEB:bool=off -DBUILD_DOCUMENTATION:bool=off -DBUILD_TESTS:bool=on
+cmake -S. -Bbuild -G Ninja -DCMAKE_BUILD_TYPE=Release -DPACK_DEB:bool=off -DBUILD_DOCUMENTATION:bool=off -DBUILD_TESTS:bool=off
 
 cmake --build build --config Release --target viewshed
 
 # testing
 # cmake --build build --config Debug --target run_tests
-cmake --build build --config Release --target run_tests
+# cmake --build build --config Release --target run_tests
 
 # install
 sudo cmake --build build --config Release --target install
