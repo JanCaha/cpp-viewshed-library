1ed4444ed024dc8dcab34f8e94cad22e9bcbec1f
Jan Caha <jan.caha@outlook.com>
2023-05-19 08:28:31 +0200

    add cell event size test

---
+++ tests/testcellevent.cpp
@@ -0,0 +1,23 @@
+#include <gmock/gmock.h>
+#include <gtest/gtest.h>
+
+#include "cellevent.h"
+
+using viewshed::CellEvent;
+
+TEST( CellEventTest, size )
+{
+    int size;
+#if ( CELL_EVENT_USE_FLOAT )
+    size = 36;
+#else
+    size = 54;
+#endif
+    ASSERT_EQ( sizeof( CellEvent ), size );
+}
+
+int main( int argc, char **argv )
+{
+    testing::InitGoogleTest( &argc, argv );
+    return RUN_ALL_TESTS();
+}
