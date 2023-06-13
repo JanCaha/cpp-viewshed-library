0c6b0f88fd28048e1545c62a22906b55a81a49d5
Jan Caha <jan.caha@outlook.com>
2023-06-13 22:31:48 +0200

    significantly update

--- cmake/FindSimpleRasters.cmake
+++ cmake/FindSimpleRasters.cmake
@@ -6,23 +6,31 @@ libfind_pkg_check_modules(simplerasters_PKGCONF simplerasters)
 # Include dir
 find_path(simplerasters_INCLUDE_DIR
     NAMES simplerasters.h
-    PATHS ${simplerasters_PKGCONF_INCLUDE_DIRS}
+    PATHS
+    /usr/include
+    /usr/include/simplerasters
+    /usr/lib64
+    "${CMAKE_PREFIX_PATH}/include"
+    ${simplerasters_PKGCONF_INCLUDE_DIRS}
 )
 
 # Finally the library itself
 find_library(simplerasters_LIBRARY
     NAMES simplerasters
-    PATHS ${simplerasters_PKGCONF_LIBRARY_DIRS}
+    PATHS
+    /usr/lib
+    /usr/lib64
+    "${CMAKE_PREFIX_PATH}/lib"
+    ${simplerasters_PKGCONF_LIBRARY_DIRS}
 )
 
-if(simplerasters_PKGCONF_FOUND)
-    message(STATUS "Library simplerasters found at: ${simplerasters_LIBRARY}.")
+if(simplerasters_LIBRARY)
+    message(STATUS "SimpleRasters installed. Found at: ${simplerasters_LIBRARY}.")
 
-    if(NOT TARGET SimpleRasters::simplerasters)
-        add_library(SimpleRasters::simplerasters UNKNOWN IMPORTED)
-        set_target_properties(SimpleRasters::simplerasters PROPERTIES
+    if(NOT TARGET SimpleRasters::SimpleRasters)
+        add_library(SimpleRasters::SimpleRasters UNKNOWN IMPORTED)
+        set_target_properties(SimpleRasters::SimpleRasters PROPERTIES
             IMPORTED_LOCATION "${simplerasters_LIBRARY}"
-            INTERFACE_INCLUDE_DIRECTORIES "${simplerasters_INCLUDE_DIR}"
-        )
+            INTERFACE_INCLUDE_DIRECTORIES "${simplerasters_INCLUDE_DIR}")
     endif()
 endif()
