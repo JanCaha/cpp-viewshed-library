16e4a170dc5d38ebab92ea414fa80b89e0bd99d2
Jan Caha <jan.caha@outlook.com>
2023-06-27 13:58:34 +0200

    fix tests

--- tests/testviewpoint.cpp
+++ tests/testviewpoint.cpp
@@ -16,12 +16,12 @@ TEST( ViewPoint, constructOk )
     std::shared_ptr<ProjectedSquareCellRaster> dem = std::make_shared<ProjectedSquareCellRaster>( TEST_DATA_DSM );
     Point vp( OGRPoint( -336404.98, -1189162.66 ), dem, 1.6 );
     ASSERT_TRUE( vp.isValid() );
-    ASSERT_NEAR( vp.x, -336404.98, 0.01 );
-    ASSERT_NEAR( vp.y, -1189162.66, 0.01 );
-    ASSERT_EQ( vp.row, 130 );
-    ASSERT_EQ( vp.col, 126 );
-    ASSERT_NEAR( vp.elevation, 1017.5416, 0.0001 );
-    ASSERT_NEAR( vp.offset, 1.6, 0.001 );
+    ASSERT_NEAR( vp.mX, -336404.98, 0.01 );
+    ASSERT_NEAR( vp.mY, -1189162.66, 0.01 );
+    ASSERT_EQ( vp.mRow, 130 );
+    ASSERT_EQ( vp.mCol, 126 );
+    ASSERT_NEAR( vp.mElevation, 1017.5416, 0.0001 );
+    ASSERT_NEAR( vp.mOffset, 1.6, 0.001 );
     ASSERT_NEAR( vp.totalElevation(), 1017.5416 + 1.6, 0.0001 );
 }
 
