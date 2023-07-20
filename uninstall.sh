35df195d54cb4ce8f6f9f37c019ee975b711c1e8
Jan Caha <jan.caha@outlook.com>
2023-07-20 14:44:13 +0200

    update scripts

---
+++ uninstall.sh
@@ -0,0 +1,3 @@
+sudo rm /usr/local/bin/inverseviewshed /usr/local/bin/viewshed /usr/local/bin/viewshedcalculator /usr/local/bin/losextractor
+sudo rm /usr/local/libviewshed.so /usr/local/libviewshed.so.2.4.0
+sudo rm -rf /usr/local/include/viewshed
