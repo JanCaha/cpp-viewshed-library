b951bd1e76902a1a4dcbcbf6582c57a9dff39bf6
Jan Caha <jan.caha@outlook.com>
2023-08-03 23:15:46 +0200

    simplify install

--- install.sh
+++ install.sh
@@ -1,39 +1,20 @@
-# sudo apt purge -y viewshed
-# sudo cmake --build build --target uninstall
 cmake --build build --target clean
 
-cmake -S. \
-    -Bbuild \
+cmake \
+    -S . \
+    -B build \
     -G Ninja \
-    -DCMAKE_CXX_COMPILER=clang++ \
-    -DCMAKE_BUILD_TYPE=Release \
-    -DPACK_DEB:bool=off \
-    -DBUILD_DOCUMENTATION:bool=off \
-    -DBUILD_TESTS:bool=on \
-    -DCELL_EVENT_DATA_FLOAT:bool=on \
-    -DOUTPUT_RASTER_DATA_FLOAT:bool=on \
-    -DNEEDS_QT:bool=off
+    -D CMAKE_CXX_COMPILER=clang++ \
+    -D CMAKE_BUILD_TYPE=Release \
+    -D PACK_DEB:bool=off \
+    -D BUILD_DOCUMENTATION:bool=off \
+    -D BUILD_TESTS:bool=off \
+    -D CELL_EVENT_DATA_FLOAT:bool=on \
+    -D OUTPUT_RASTER_DATA_FLOAT:bool=on \
+    -D NEEDS_QT:bool=on
 
 cmake --build build --config Release --target library_viewshed
-
-# testing
-# cmake --build build --config Debug --target run_tests
-# cmake --build build --config Release --target all
-# cmake --build build --config Release --target run_tests
+cmake --build build --config Release --target create_binaries
 
 # install
 sudo cmake --build build --config Release --target install
-
-# pack DEB
-# cmake -S. -Bbuild -G Ninja -DCMAKE_BUILD_TYPE=Release -DPACK_DEB:bool=on -DBUILD_DOCUMENTATION:bool=off -DBUILD_TESTS:bool=off
-# cmake --build build --config Release --target all
-# cmake --build build --config Release --target pack_viewshed_library_deb
-
-# sudo cmake --build build --target clean
-# cmake -S. -Bbuild -G Ninja -DCMAKE_BUILD_TYPE=Release \
-#     -DPACK_DEB:bool=off \
-#     -DBUILD_DOCUMENTATION:bool=off \
-#     -DBUILD_TESTS:bool=on \
-#     -DCELL_EVENT_DATA_FLOAT:bool=off \
-#     -DOUTPUT_RASTER_DATA_FLOAT:bool=off
-# cmake --build build --config Release --target run_tests
