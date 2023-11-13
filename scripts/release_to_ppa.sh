4d409aaaa5f1838c3b6ce8d6a86e2190ac9aae60
Jan Caha <jan.caha@outlook.com>
2023-11-13 17:41:06 +0100

    use debian numbering

--- scripts/release_to_ppa.sh
+++ scripts/release_to_ppa.sh
@@ -7,12 +7,14 @@ PWD=`pwd`
 
 FOLDER=cpp-viewshed-library
 PACKAGE=viewshed
-VERSION=$(grep "project("  CMakeLists.txt | grep -E -o -e "[0-9\.]+" | head -n 1)
+MAIN_VERSION=$(grep "project("  CMakeLists.txt | grep -E -o -e "[0-9\.]+" | head -n 1)
+DEBIAN_VERSION=$(grep "viewshed (" debian/changelog -m 1 |  grep -E -o -e "[0-9]+\.[0-9]+\.[0-9]+(-[0-9]+)?")
 
 rm -rf build
 cd ..
-tar -acf "$PACKAGE"_"$VERSION".orig.tar.gz $FOLDER
+tar -acf "$PACKAGE"_"$MAIN_VERSION".orig.tar.gz $FOLDER
 cd $FOLDER
 debuild -S -sa
 cd ..
-dput ppa:jancaha/gis-tools "$PACKAGE"_"$VERSION"-0ppa0_source.changes
+dput ppa:jancaha/gis-tools "$PACKAGE"_"$DEBIAN_VERSION"ppa0_source.changes
+rm $PACKAGE*
