0e90fa5e0783f8ca9019e309941bf68987cbcbc4
Jan Caha <jan.caha@outlook.com>
2023-11-13 19:34:00 +0100

    use versions

--- scripts/release_to_ppa.sh
+++ scripts/release_to_ppa.sh
@@ -7,14 +7,13 @@ PWD=`pwd`
 
 FOLDER=cpp-viewshed-library
 PACKAGE=viewshed
-MAIN_VERSION=$(grep "project("  CMakeLists.txt | grep -E -o -e "[0-9\.]+" | head -n 1)
-DEBIAN_VERSION=$(grep "viewshed (" debian/changelog -m 1 |  grep -E -o -e "[0-9]+\.[0-9]+\.[0-9]+(-[0-9]+)?")
+VERSION=$(grep "project("  CMakeLists.txt | grep -E -o -e "[0-9\.]+" | head -n 1)
 
 rm -rf build
 cd ..
-tar -acf "$PACKAGE"_"$MAIN_VERSION".orig.tar.gz $FOLDER
+tar -acf "${PACKAGE}"_"${VERSION}".orig.tar.gz $FOLDER
 cd $FOLDER
-debuild -S -sa
+debuild -S -sa -d
 cd ..
-dput ppa:jancaha/gis-tools "$PACKAGE"_"$DEBIAN_VERSION"ppa0_source.changes
-rm $PACKAGE*
+dput ppa:jancaha/gis-tools "$PACKAGE"_"$VERSION"-0ppa0_source.changes
+# rm $PACKAGE*
