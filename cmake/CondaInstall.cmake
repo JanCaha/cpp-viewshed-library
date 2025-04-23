d4bcf17cc62a48b4816b888e3d1e312c0cdec383
Jan Caha <jan.caha@outlook.com>
2025-04-23 19:31:17 +0200

    only use some paths

--- cmake/CondaInstall.cmake
+++ cmake/CondaInstall.cmake
@@ -6,20 +6,14 @@ if(DEFINED ENV{CONDA_BUILD} AND WIN32)
     set(BUILD_SHARED_LIBS ON CACHE BOOL "Build shared libraries" FORCE)
 
     install(TARGETS library_viewshed
-        COMPONENT lib
         RUNTIME DESTINATION $ENV{LIBRARY_BIN}
-        LIBRARY DESTINATION $ENV{LIBRARY_LIB}
         PUBLIC_HEADER DESTINATION $ENV{LIBRARY_INC}/${LIBRARY_NAME}
-        PRIVATE_HEADER DESTINATION $ENV{LIBRARY_INC}/${LIBRARY_NAME}
-        INCLUDES DESTINATION $ENV{LIBRARY_INC}/${LIBRARY_NAME}
     )
 
     if(HAS_QT)
         install(TARGETS
             viewshed inverseviewshed viewshedcalculator losextractor viewshed_widgets
-            COMPONENT bin
             RUNTIME DESTINATION $ENV{LIBRARY_BIN}
-            LIBRARY DESTINATION $ENV{LIBRARY_LIB}
         )
     endif()
 else()
