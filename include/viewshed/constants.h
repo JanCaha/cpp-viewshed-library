29caaef10144722f564e4252ebeb483fe00e9235
Jan Caha <jan.caha@outlook.com>
2025-05-31 22:52:46 +0200

    remove export

--- include/viewshed/constants.h
+++ include/viewshed/constants.h
@@ -1,9 +1,7 @@
 #pragma once
 
-#include "viewshed_export.h"
-
 namespace viewshed
 {
-    extern DLL_API const double REFRACTION_COEFFICIENT{ 0.142860 };
-    extern DLL_API const double EARTH_DIAMETER{ 12740000 };
+    const double REFRACTION_COEFFICIENT{ 0.142860 };
+    const double EARTH_DIAMETER{ 12740000 };
 } // namespace viewshed
