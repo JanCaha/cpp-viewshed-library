3cdb1f58c9f087759cb3c2c5bb25a7f89891bfcb
JanCaha <JanCaha@users.noreply.github.com>
2025-06-24 22:32:04 +0000

    deploy: 9f0e54831c572051e850db7f07270afe993683cf

--- search/all_11.js
+++ search/all_11.js
@@ -2,8 +2,8 @@ var searchData=
 [
   ['valid_0',['valid',['../classBS_1_1multi__future.html#a39a5321e6be74bf09cb03514bf7cd42b',1,'BS::multi_future']]],
   ['validateraster_1',['validateRaster',['../classviewshed_1_1ViewshedUtils.html#aeb58168a70796031981d5030a35a87f4',1,'viewshed::ViewshedUtils']]],
-  ['value_2',['value',['../classviewshed_1_1LoSNode.html#a2e72c953395852d70e639e789da1ef0f',1,'viewshed::LoSNode']]],
-  ['valueatangle_3',['valueAtAngle',['../classviewshed_1_1LoSNode.html#ae43f3ed1ff5ee6d3887b0816b3a4ffed',1,'viewshed::LoSNode']]],
+  ['value_2',['value',['../classviewshed_1_1LoSNode.html#afed51bbeeca5504d3e4c721d5280d1e8',1,'viewshed::LoSNode']]],
+  ['valueatangle_3',['valueAtAngle',['../classviewshed_1_1LoSNode.html#a4f2cada0f5f40379fe486335ad7dbe10',1,'viewshed::LoSNode']]],
   ['viewangle_4',['ViewAngle',['../classviewshed_1_1visibilityalgorithm_1_1ViewAngle.html',1,'viewshed::visibilityalgorithm']]],
   ['viewpointelevation_5',['viewPointElevation',['../classviewshed_1_1AbstractLoS.html#aae235d06f63a0f9c48b73ef8368b1bbc',1,'viewshed::AbstractLoS']]],
   ['viewpointtotalelevation_6',['viewPointTotalElevation',['../classviewshed_1_1AbstractLoS.html#a8c42883b7cb1aee225fd9cba9f724496',1,'viewshed::AbstractLoS']]],
